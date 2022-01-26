a5d90029a4d3e859b37dc7b6e0eecd084cbdf148
root <root@DESKTOP-N8RER9V.localdomain>
2022-01-26 01:06:16 +0000

    remove commented code in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,10 @@
 //load in bootstrap js
 import * as Vue from 'vue';
-//import components from './components/';
 
 require('./bootstrap');
 //custom event listeners
 require('./EventListeners/Listeners');
 
-//import { Plugin } from "vue-fragment";
-//Vue.use(Plugin);
 
 import Navigation from './components/Navigation';
 import NavList from "./components/NavList";
@@ -20,19 +17,5 @@ app.component('header-component', HeaderComponent)
 app.component('nav-list', NavList);
 app.component('navigation', Navigation);
 app.component('nav-burger-button', NavBurgerButton);
-
-
-
-//app.use(components);
-//app.use(components);
-// Vue.component('navigation', Navigation);
-// Vue.component('nav-list', NavList);
-// Vue.component('nav-burger-button', NavBurgerButton);
-// Vue.component('header-component', Header);
-
-//initialise vue
-// const app = new Vue({
-//     el: '#app',
-// });
 
 app.mount("#app");
